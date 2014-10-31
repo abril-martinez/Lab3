bc50d16d7182cd8be2ae4915ea624d41af0bb98a
abril-martinez <amartinez28@email.arizona.edu>
2014-10-31 05:21:15 -0700

    Updated pwm and adc segments

--- Lab3.c
+++ Lab3.c
@@ -12,17 +12,13 @@
 #define PWM_FREQ                100
 #define PR_VALUE                (57600/PWM_FREQ)-1 //for prescaler of 1:256
 
-#define TRIS_MOTOR_1            TRISAbits.TRISA0
-#define TRIS_MOTOR_2            TRISAbits.TRISA1
-#define LAT_MOTOR_1             LATAbits.LATA0
-#define LAT_MOTOR_2             LATAbits.LATA1  
-
-#define TRIS_POT                TRISBbits.TRISB3   //(RB3 -> I/O 6)
-#define AN_POT                  AD1PCFGbits.PCFG5 
-
+#define TRIS_MOTOR_D            TRISBbits.TRISB2 // back left wheel
+#define TRIS_MOTOR_C            TRISBbits.TRISB3 // back right wheel
+#define LAT_MOTOR_D             LATBbits.LATB2
+#define LAT_MOTOR_C             LATBbits.LATB3  
 
 volatile int ADC_value;
-volatile char count;
+volatile int Forward_Backward_State;
 volatile char done;
 
 // ******************************************************************************************* //
@@ -45,35 +41,55 @@ _CONFIG2( IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & O
 		 IOL1WAY_OFF & I2C1SEL_PRI & POSCMOD_XT )
 
 
-/************************************A/D Conversion********************************************/
-
 void PWMInitialize() {
-    //Set up Timer 2 for PWM
+    
+    /**********************Timer2****************************/
     TMR2 = 0;
-    PR2 = PR_VALUE; // (1/100) / (1/(Fcy)   * 256)
+    PR2 = PR_VALUE; // 575
     T2CON = 0x0030; // prescale 256
-	T2CONbits.TON = 1; // turn timer1 on	
    
+    //Set up pin RP8 for PWM, using OC1 -- LEFT PWM
+    RPOR4bits.RP8R =  18; // OC1 using RP8
+    OC1R = 0; // PR2 * Duty cycle (initialized to 0%)
+    OC1RS = 0;
+    OC1CON = 0x0006; // Timer2
+
+    //set pin RP9 for PWM, using OC2 -- RIGHT PWM
+    RPOR4bits.RP9R = 19; // OC2 using RP9
+    OC2R = 0; // PR2 * Duty cycle, (initialized to 0%)
+    OC2RS = 0;
+    OC2CON = 0x0006;
+
+	T2CONbits.TON = 1; // turn timer2 on	
+
+    /**********************Timer3****************************/
+	
+    TMR3 = 0;
+    PR3 = PR_VALUE; // 575
+    T3CON = 0x0030; // prescale 256
+
     //Set up pin RP8 for PWM, using OC1 -- LEFT PWM
     RPOR4bits.RP8R =  18; //OC1 using RP8
     OC1R = 0; // PR1 * Duty cycle, which is initialized to 0%
     OC1RS = 0;
-    OC1CON = 0x0006; //Timer2
+    OC1CON = 0x000E; //Timer3
 
     //Set up pin RP9 for PWM, using OC2 -- RIGHT PWM
     //RPOR4bits.RP9R =  19; //OC2 using RP9
     RPOR5bits.RP10R = 19; //OC2 using RP10 (RB10 -> I09)
     OC2R = 0; // PR2 * Duty cycle, which is initialized to 0%
     OC2RS = 0;
-    OC2CON = 0x0006;
+    OC2CON = 0x000E; // Timer3
+
+	T3CONbits.TON = 1; // turn timer1 on
 }
 
 void ADCInitialize(){
-	AD1PCFG &= 0xFFDF;      // AN5 (RB3->IO6) input pin is analog
+	AD1PCFG &= 0xFFFE;      // AN0 input pin is analog
 	AD1CON2 = 0;          	// use MUXA, AVss and AVdd are used as Vref+/-
 	AD1CON3 = 0x0101;     	// set TAD to 1, 2*TCY	
   	AD1CON1 = 0x20E4;     	// auto convert after end of sampling
-	AD1CHS = 0x0005;        // Cofnigure input channels (AN5 ->I/O pin 6 ->RB3)  
+	AD1CHS = 0;        		// Cofnigure input channels (AN5 ->I/O pin 6 ->RB3)  
 	AD1CSSL = 0;          	// no input is scanned
 
 	// Configure A/D interrupt
@@ -85,119 +101,122 @@ void ADCInitialize(){
 
 int main(void)
 {
-    double	AD_value;
 	char value[8];
-    count=0;
-
+	Forward_Backward_State;
+	
 	// Set the directional pins
-	TRIS_POT = 1;      // RB3 Potentiometer (I/O 6)
-	AN_POT = 0;        // RB3 set analog
-    TRIS_MOTOR_1 = 0;  // RA0
-    TRIS_MOTOR_2 = 0;  // RA1
-    LAT_MOTOR_1 = 1;   // idle mode
-    LAT_MOTOR_2 = 1;   // idle mode
+    TRIS_MOTOR_C = 0;  // RB2
+    TRIS_MOTOR_D = 0;  // RB3
+    LAT_MOTOR_C = 1;   // idle mode
+    LAT_MOTOR_D = 1;   // idle mode
 
 	// Motor related variables	
-	double duty_cycle1;
-    double duty_cycle2;
+	int duty_cycle1 = 0;
+    int duty_cycle2 = 0;
 
     // Configure Change notification of RB5 (SW1 momentary switch)
-	TRISBbits.TRISB5 = 1;	
+	TRISBbits.TRISB5 = 1;
+	CNPU2bits.CN27PUE=1;	
     CNEN2bits.CN27IE=1;
-    CNPU2bits.CN27PUE=1;
     IFS1bits.CNIF=0;
     IEC1bits.CNIE=1;
 
 	// Initialize LCD
     LCDInitialize();
 
-	// Configure A/D unit
-    ADCInitialize(); 
-
 	// Configure PWM
-    PWMInitialize();	
+    PWMInitialize();
+
+	// Configure A/D unit
+    ADCInitialize(); 	
 
 	while(1) 
       {		
 	  if(done)
 		{
-		  sprintf(value, "%6d", ADC_value);
-    	  LCDMoveCursor(0,0);
-          LCDPrintString(value);
+		 sprintf(value, "%6d", ADC_value);
+    	 LCDMoveCursor(0,0);
+         LCDPrintString(value);
+		
 
-		  AD_value = (ADC_value * 3.3)/1024;
-
-			if(AD_value >= 1.65) 
-			{
-				duty_cycle1 = 1; //motor 1 is on full from 1.65-3.3
-				duty_cycle2 = (AD_value/(-1.65))+2; //motor 2 decreases
-			}
+	/*	 if(AD_value >= 1.65) 
+		  {
+		   duty_cycle1 = 1; //motor 1 is on full from 1.65-3.3
+		   duty_cycle2 = (AD_value/(-1.65))+2; //motor 2 decreases
+		  }
 			else
 			{
 				duty_cycle1 = (AD_value/1.65); //motor 1 decreases
 				duty_cycle2 = 1; //motor 2 on full from 0-1.65
-			}
+			} */
+			
+
 			OC1RS = (int)(PR_VALUE*duty_cycle1); //modifying PWM duty cycle
 			OC1RS = OC1RS / 100;
 			OC2RS = (int)(PR_VALUE*duty_cycle2); //modifying PWM duty cycle
 			OC2RS = OC2RS / 100;
-			sprintf(value, "%.2f", duty_cycle1);
-			LCDMoveCursor(1,0);LCDPrintString(value);
-			sprintf(value, "%.2f", duty_cycle2);
-			LCDMoveCursor(1,4);LCDPrintString(value);
+			sprintf(value, "%3d", duty_cycle1);
+			LCDMoveCursor(1,0);
+			LCDPrintString(value);
+			sprintf(value, "%3d", duty_cycle2);
+			LCDMoveCursor(1,5);
+			LCDPrintString(value);  
 			
 			done = 0;
 		} 
 	  }
-
 	return 0;
 	}
-
-
 
 void __attribute__((interrupt,auto_psv)) _CNInterrupt(void)
 {	
 	IFS1bits.CNIF = 0;
 	
-    //switch from idle, forward, idle, backward, idle modes on button release
-	if(PORTBbits.RB5 == 1)
-	{
-		if(count == 0)//move forwards
-		{
-			LATAbits.LATA0 = 0;	
-			LATAbits.LATA1 = 1;
-		}
-		else if(count == 1) //idle1
-		{
-			LATAbits.LATA0 = 0;	
-			LATAbits.LATA1 = 0;	
-		}
-		else if(count == 2) //move backwards
-		{
-			LATAbits.LATA0 = 1;	
-			LATAbits.LATA1 = 0;	
-		}
-		else if(count == 3) //idle2
-		{
-			LATAbits.LATA0 = 0;	
-			LATAbits.LATA1 = 0;
-			count=-1;	//reset count variable
-		}
-		
-		count++;
-	}
-	
-}
+   // transition to next Forward_Backward_State when SW1 is pressed
+    if(PORTBbits.RB5 == 0)
+      {
+       Forward_Backward_State++;
+         if(Forward_Backward_State  > 4)
+           {
+            Forward_Backward_State = 1;
+           }
+      } 
+
+	switch(Forward_Backward_State){
+        
+		//idle1: both motors stopped
+        case 1:
+            LAT_MOTOR_D = 1;
+            LAT_MOTOR_C = 1;
+            break;
+        
+		//forward: both motors moves in a forward direction
+        case 2:
+            LAT_MOTOR_D = 0;
+            LAT_MOTOR_C = 1;
+            break;
+        
+		//idle2: both motors stopped
+        case 3:
+            LAT_MOTOR_D = 1;
+            LAT_MOTOR_C = 1;
+            break;
+        
+		//backward:both motors move in a backward direction
+        case 4:
+            LAT_MOTOR_D = 1;
+            LAT_MOTOR_C = 0;
+            break;
+
+    }
+}	
 
 // ******************************************************************************************* //
-void  __attribute__((interrupt,auto_psv)) _ADC1Interrupt(void) 
-{	
-   IFS0bits.AD1IF = 0;
-   ADC_value = ADC1BUF0;
-   done = 1;
-} 
-
-
-
+void  __attribute__((interrupt,auto_psv)) _ADC1Interrupt(void)
+{
+ IFS0bits.AD1IF = 0;
+ ADC_value = ADC1BUF0;
+ done = 1;
+}
 
 
